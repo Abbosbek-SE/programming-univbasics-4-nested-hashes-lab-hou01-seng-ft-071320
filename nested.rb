ab499f82da4d79f8186c47650fbf2427c9f07981
Abbosbek Muydinboev <muydinboevabbosbek@gmail.com>
2020-06-26 05:58:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,11 +63,6 @@ end
 
 def adding_matz
   
-   programmer_hash[:yukihiro_matsumoto] = {
-      :known_for => "Ruby",
-      :languages => ["LISP", "C"]
-    }
-   
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -83,6 +78,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
+    
+    programmer_hash[:yukihiro_matsumoto] = {
+      :known_for => "Ruby",
+      :languages => ["LISP", "C"]
+    }
+   
     return programmer_hash
 end
 
